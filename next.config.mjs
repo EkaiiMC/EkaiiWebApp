59273d36536c5e82007d86f32f505237ac1166d6
Quentin GAVOILLE <44027243+QGavoille@users.noreply.github.com>
2024-07-21 22:43:22 +0200

    feat(next-config): update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                "protocol": "https",
+                "hostname": "mc-heads.net",
+            },
+        ],
+    },
+    reactStrictMode: true,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.plugins.push(
+            new webpack.IgnorePlugin({ resourceRegExp: /osx-temperature-sensor/ })
+        );
+        // Enable polling based on env variable being set
+        if(process.env.NEXT_WEBPACK_USEPOLLING) {
+            config.watchOptions = {
+                poll: 500,
+                aggregateTimeout: 300
+            }
+        }
+        return config
+    },
+};
 
 export default nextConfig;
