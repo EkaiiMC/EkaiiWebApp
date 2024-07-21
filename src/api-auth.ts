caa5c39ab8b902af153495c70ee1014122148691
Quentin GAVOILLE <44027243+QGavoille@users.noreply.github.com>
2024-07-21 22:42:50 +0200

    feat(db): add db

---
+++ src/api-auth.ts
@@ -0,0 +1,94 @@
+import prisma from "@/db";
+import bcrypt from 'bcryptjs'
+
+export interface ApiScope {
+  [key: string]: ApiScope | boolean;
+}
+
+export async function generateApiKey(name: string, scopes: ApiScope) : Promise<{ key: string, scopes: ApiScope }> {
+  const saltRounds = 10;
+  const token = crypto.randomUUID();
+  const hashedToken = await bcrypt.hash(token, saltRounds);
+  await prisma.apiKey.create({
+    data: {
+      key: hashedToken,
+      name,
+      scopes
+    }
+  })
+  return {key: token, scopes}
+}
+
+export async function editPermissions(name: string, newScopes : ApiScope) {
+  const record = await prisma.apiKey.findFirst({where: {name: name}})
+  if (!record) throw new Error("Api Key not found")
+  await prisma.apiKey.update({
+    where: {
+      name
+    },
+    data: {
+      scopes: newScopes
+    }
+  })
+}
+
+export async function deleteApiKey(name: string) {
+  if(!(await prisma.apiKey.delete({
+    where: {
+      name
+    }
+  }))) throw new Error("Api Key not found")
+}
+
+export async function getPermissions(name: string) : Promise<ApiScope> {
+  const key = await prisma.apiKey.findFirst({where: {name}});
+  if (!key) throw new Error("Api Key not found")
+  return key.scopes as ApiScope;
+}
+
+export async function getKeys() : Promise<string[]> {
+  return (await prisma.apiKey.findMany()).map((e) => e.name);
+}
+
+export async function checkAccess(token: string, neededPermission : string) : Promise<boolean> {
+  const keys = await prisma.apiKey.findMany()
+  for (const key of keys) {
+    if (await bcrypt.compare(token, key.key)) {
+      return hasPermission(key.scopes as ApiScope, neededPermission)
+    }
+  }
+  throw new Error("Api Key not found")
+}
+
+export function hasPermission(scope: ApiScope, permission: string): boolean {
+  const parts = permission.split('.');
+  let currentScope: ApiScope | boolean = scope;
+  for (const part of parts) {
+    if (typeof currentScope === 'boolean') {
+      return currentScope;
+    }
+    if (currentScope[part] === undefined) {
+      return false;
+    }
+    currentScope = currentScope[part];
+  }
+  return currentScope === true;
+}
+
+const roles = ['USER', 'MEMBER', 'WHITELISTER', 'DESIGNER', 'DEVELOPER', 'MAINTAINER']
+
+export function isWhitelisterOrMore(role: string) {
+  return roles.indexOf(role) >= roles.indexOf('WHITELISTER')
+}
+
+export function isDesignerOrMore(role: string) {
+  return roles.indexOf(role) >= roles.indexOf('DESIGNER')
+}
+
+export function isDeveloperOrMore(role: string) {
+  return roles.indexOf(role) >= roles.indexOf('DEVELOPER')
+}
+
+export function isMaintainer(role: string) {
+  return roles.indexOf(role) >= roles.indexOf('MAINTAINER')
+}
---
+++ src/auth.ts
@@ -0,0 +1,125 @@
+import NextAuth from 'next-auth'
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import MicrosoftEntraID from "@auth/core/providers/microsoft-entra-id";
+import prisma from "@/db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [MicrosoftEntraID({
+    tenantId: '9188040d-6c67-4c5b-b112-36a304b66dad',
+    authorization: {
+      params: {
+        scope: 'openid email XboxLive.signin',
+      },
+    },
+    async profile(profile, tokens) {
+      if(tokens.id_token) {
+        const decoded = JSON.parse(Buffer.from(tokens.id_token.split('.')[1], 'base64').toString('utf-8')) as {email: string}
+        const xbox = await fetch(
+          'https://user.auth.xboxlive.com/user/authenticate',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+            },
+            body: JSON.stringify({
+              RelyingParty: 'http://auth.xboxlive.com',
+              TokenType: 'JWT',
+              Properties: {
+                AuthMethod: 'RPS',
+                SiteName: 'user.auth.xboxlive.com',
+                RpsTicket: `d=${tokens.access_token}`,
+              },
+            }),
+          }
+        );
+
+        if (xbox.ok) {
+          const xboxRes = (await xbox.json()) as {Token?: string, DisplayClaims: {xui: {uhs?: string}[]}};
+
+          const xboxToken = xboxRes.Token;
+          const xboxUserHash = xboxRes.DisplayClaims.xui[0].uhs;
+
+          if (xboxToken && xboxUserHash) {
+            const xsts = await fetch(
+              'https://xsts.auth.xboxlive.com/xsts/authorize',
+              {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  Accept: 'application/json',
+                },
+                body: JSON.stringify({
+                  Properties: {
+                    SandboxId: 'RETAIL',
+                    UserTokens: [xboxToken],
+                  },
+                  RelyingParty: 'rp://api.minecraftservices.com/',
+                  TokenType: 'JWT',
+                }),
+              }
+            );
+
+            if(xsts.ok) {
+              const xstsRes = (await xsts.json()) as {Token?: string};
+
+              if(xstsRes.Token) {
+                const minecraft = await fetch(
+                  'https://api.minecraftservices.com/authentication/login_with_xbox',
+                  {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      Accept: 'application/json',
+                    },
+                    body: JSON.stringify({
+                      identityToken: 'XBL3.0 x='+xboxUserHash+';'+xstsRes.Token,
+                      ensureLegacyEnabled: true,
+                    }),
+                  }
+                );
+
+                if(minecraft.ok) {
+                  const minecraftRes = (await minecraft.json()) as {access_token: string};
+
+                  if(minecraftRes.access_token) {
+                    const minecraftProfile = await fetch(
+                      'https://api.minecraftservices.com/minecraft/profile',
+                      {
+                        headers: {
+                          Authorization: `Bearer ${minecraftRes.access_token}`,
+                        },
+                      }
+                    );
+
+                    if(minecraftProfile.ok) {
+                      const minecraftProfileRes = (await minecraftProfile.json()) as { id: string, name: string };
+
+                      return {
+                        id: minecraftProfileRes.id,
+                        name: minecraftProfileRes.name,
+                        email: decoded.email,
+                        image: 'https://mc-heads.net/avatar/'+minecraftProfileRes.name+'/256',
+                        role: 'USER'
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      throw new Error('Failed to fetch Minecraft profile')
+    },
+  })],
+  callbacks: {
+    async session({session, user}) {
+      // @ts-ignore
+      session.user.role = user.role;
+      return session;
+    },
+  }
+})
