5690a827f6a402d1c659ade13de7124f234bc070
Quentin GAVOILLE <quentin.gavoille@gmail.com>
2024-11-28 12:51:30 +0100

    unused import

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,5 @@
 import {ReactNode} from "react";
 import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 export default function PageLayout({children}: { children: ReactNode }) {
   return (
