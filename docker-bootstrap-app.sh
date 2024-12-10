c50621cbcd963affb0725f13ad14783a0056bb0c
Quentin GAVOILLE <quentin.gavoille@gmail.com>
2024-12-10 18:32:02 +0100

    fix docker

--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-DATABASE_URL="postgres://ekaii:ekaii@db:5432/ekaii" npx prisma migrate deploy
+DATABASE_URL="postgres://ekaii:ekaii@db:5432/ekaii" yarn prisma migrate deploy
 DATABASE_URL="postgres://ekaii:ekaii@db:5432/ekaii" node server.js
