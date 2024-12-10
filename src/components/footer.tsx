c63a268d36e3d2571fffe5bd128da5abadd310e0
Quentin GAVOILLE <quentin.gavoille@gmail.com>
2024-12-10 18:22:09 +0100

    fix text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="relative flex justify-between items-center h-16 bg-bgDarkGray text-white py-10 text-center text-[12pt] sm:px-10 font-extralight w-full sm:text-lg sm:text-left mt-auto">
-      <p>Copyright © 2024 EkaiiMC - Tous droits réservés - Non affilié à Mojang ou Microsoft - <Link href={'https://www.serveurs-minecraft.org'}>serveurs-minecraft.org</Link></p>
+      <p>Copyright © 2024 EkaiiMC - Tous droits réservés - Non affilié à Mojang ou Microsoft - <Link href={'http://www.serveurs-minecraft.org'}>Serveur Minecraft</Link></p>
     </footer>
   )
 }
